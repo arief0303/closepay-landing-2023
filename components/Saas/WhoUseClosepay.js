316266dd958a74c64c9db7baaf5b628660876e8e
Arief R <arief0303@gmail.com>
2023-03-29 15:13:10 +0700

    "Who use closeplay" section

--- components/Saas/WhoUseClosepay.js
+++ components/Saas/WhoUseClosepay.js
@@ -6,52 +6,51 @@ const WorkProcess = () => {
       <section className="saas-work-process ptb-100">
         <div className="container">
           <div className="section-title">
-            <h2>Work Process</h2>
+            <h2>Who Use Closepay?</h2>
             <div className="bar"></div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
+            <p>Institutions, community or local government
+              with a large number of users need a digital
+              transaction to support their internal ecosystem.</p>
           </div>
 
           <div className="row">
             <div className="col-lg-3 col-md-6">
               <div className="single-work-process">
                 <div className="icon">
-                  <i className="icofont-calendar"></i>
+                  <i className="icofont-institution"></i>
                 </div>
-                <h3>Plan</h3>
-                <p>Morbi pharetra sapien ut mattis.</p>
+                <h3>Government</h3>
+                <p>Governments especially in Indonesia can use our self managed e-money system with no effort, all the transactions, system and legal requerement are handled by us.</p>
               </div>
             </div>
 
             <div className="col-lg-3 col-md-6">
               <div className="single-work-process">
                 <div className="icon">
-                  <i className="icofont-code-alt"></i>
+                  <i className="icofont-hat-alt"></i>
                 </div>
-                <h3>Develop</h3>
-                <p>Morbi pharetra sapien ut mattis.</p>
+                <h3>Institution</h3>
+                <p>Anually billing for the college student? Food court payment? all is easily comes with Closepay. All the money cashflow will directly to institution bank account, no worries to have a partnership with us!</p>
               </div>
             </div>
 
             <div className="col-lg-3 col-md-6">
               <div className="single-work-process">
                 <div className="icon">
-                  <i className="icofont-server"></i>
+                  <i className="icofont-building-alt"></i>
                 </div>
-                <h3>Lounch</h3>
-                <p>Morbi pharetra sapien ut mattis.</p>
+                <h3>Company</h3>
+                <p>Salary distribition? Employee lending? All your requirements can be customeable with Closepay, got the class!</p>
               </div>
             </div>
 
             <div className="col-lg-3 col-md-6">
               <div className="single-work-process">
                 <div className="icon">
-                  <i className="icofont-check"></i>
+                  <i className="icofont-people"></i>
                 </div>
-                <h3>Reporting</h3>
-                <p>Morbi pharetra sapien ut mattis.</p>
+                <h3>Community</h3>
+                <p>Membership System Many kinds of Clubs.</p>
               </div>
             </div>
           </div>
